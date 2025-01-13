846fdc7c3a6807c79b44dfcf513b45c1d7cb4263
h-satoshi <h8-shnagasaki@au.com>
2025-01-14 03:36:09 +0900

    1/14

---
+++ src/main/java/com/diworksdev/ecsite/util/DBConnector.java
@@ -0,0 +1,21 @@
+package com.diworksdev.ecsite.util;
+
+import java.sql.Connection;
+
+public class DBConnector {
+	
+	private static String driverName = "com.mysql.jdbc.Dricver";
+	
+	private static String url = "jdbc:mysql://localhost/ecsite";
+	
+	private static String user = "root";
+	
+	private static String password = "";
+	
+	public Connection getConnection() {
+		
+		
+		
+	}
+
+}
