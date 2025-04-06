a625377d8a56c7388dab95c0d281e0a707edab74
h-satoshi <h8-shnagasaki@au.com>
2025-04-07 02:17:13 +0900

    4/7書直し

---
+++ src/main/java/com/diworksdev/ecsite/dao/BuyItemDAO.java
@@ -0,0 +1,35 @@
+package com.diworksdev.ecsite.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.diworksdev.ecsite.dto.BuyItemDTO;
+import com.diworksdev.ecsite.util.DBConnector;
+
+public class BuyItemDAO {
+	private DBConnector dbConnector = new DBConnector();
+	private Connection connection = dbConnector.getConnection();
+	private BuyItemDTO buyItemDTO = new BuyItemDTO();
+	
+	public BuyItemDTO getBuyItemInfo() {
+		//商品情報をすべて取得するSQL
+		String sql = "SELECT id, item_name, item_price FROM item_info_transaction";
+		
+		try {
+			PreparedStatement preparedStatement = connection.prepareStatement(sql);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			if (resultSet.next()) {
+				//DBから取得した情報をDTOクラスに格納する
+				buyItemDTO.setId(resultSet.getInt("id"));
+				buyItemDTO.setItemName(resultSet.getString("item_name"));
+				buyItemDTO.setItemPrice(resultSet.getString("item_price"));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//ActionクラスにDTOクラスに返す
+		return buyItemDTO;
+	}
+}
